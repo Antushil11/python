75eb1ab8fd65cab83e28c093ac3fa57175ba7b17
ANTU SHIL <anturoy084@gmail.com>
2026-06-08 20:51:52 +0600

    added assignment 2

--- Chapter 1/Assignment2.py
+++ Chapter 1/Assignment2.py
@@ -1,1 +1,11 @@
 #question take diameter as input and calculate area of circle
+
+diameter = int (input("Enter the the value :"))
+
+radius =(diameter) / 2
+
+area = 3.14 * (radius ** 2)
+
+print ("The value of area is :", area)
+
+
