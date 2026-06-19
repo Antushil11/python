828e41410f007302526d6c809e8d7a999aa6fcb0
ANTU SHIL <anturoy084@gmail.com>
2026-06-19 20:55:59 +0600

    added tuple

---
+++ Chapter4/tupleBasic.py
@@ -0,0 +1,16 @@
+# tuples Basic
+numberTuple = (87,32,45,56)
+textTuple=("babul","cabul","kabul","mabul")
+
+
+
+print(type(textTuple))
+
+
+empty=()
+singleTuple=(1,)
+print(type(empty))
+print(type(singleTuple))
+
+
+
