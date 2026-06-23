2edd51cbc40e373482a63782d898d9c678e687e6
ANTU SHIL <anturoy084@gmail.com>
2026-06-23 20:26:59 +0600

    added assignmnet13

---
+++ Chapter5/assignment13.py
@@ -0,0 +1,12 @@
+# Create a dictionary storing meanings of 3 English words.
+english ={
+    "happy" : "Now I am Happy",
+    "brave": "ready to face danger or pain without fear",
+    "quick": "moving fast or doing something in a short time"
+}
+
+print(english["brave"])
+
+
+
+
